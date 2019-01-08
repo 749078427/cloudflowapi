4484a1a641aafed368b18b8d46c0660c6fe97e74
749078427 <749078427@qq.com>
2019-01-08 10:59:07 +0800

    test

---
+++ src/main/java/com/utter/CloudflowapiApplication.java
@@ -0,0 +1,14 @@
+package com.utter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudflowapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudflowapiApplication.class, args);
+	}
+
+}
+
